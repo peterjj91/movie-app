da9eb57dc9cfea8fbb1d2505ded8d1c71bc8ba39
Popeskul Pavel <peterjj91@gmail.com>
2019-10-11 22:21:36 +0300

    Add api key

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://api.themoviedb.org/3';
+
+export const API_KEY_3 = '1b65d0b8218370f2ca901397cbb76132';
+
+export const API_KEY_4 =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjY1ZDBiODIxODM3MGYyY2E5MDEzOTdjYmI3NjEzMiIsInN1YiI6IjVkYTBiMzI4YWUzNjY4MDAxMzIxNTRhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ncLEPY-yeDCBxr2UeoXAxA9jw7SRTwcVHZ-LABY-_44';
