706480920a0ced2d5daf28e46d963f2d30855d9a
Popeskul Pavel <peterjj91@gmail.com>
2019-10-19 13:26:24 +0300

    Chain Promises for Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,10 +4,30 @@ import { API_URL, API_KEY_3 } from './../../api/api';
 
 export default class Login extends Component {
   sendPromises = () => {
-    fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
-      .then(response => response.json())
+    const fetchApi = (url, options = {}) => {
+      return new Promise((resolve, reject) => {
+        fetch(url, options)
+          .then(response => {
+            if (response.status < 400) {
+              return response.json();
+            } else {
+              throw response;
+            }
+          })
+          .then(data => {
+            resolve(data);
+          })
+          .catch(response => {
+            response.json().then(error => {
+              reject(error);
+            });
+          });
+      });
+    };
+
+    fetchApi(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
       .then(data => {
-        fetch(
+        return fetchApi(
           `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`,
           {
             method: 'POST',
@@ -21,27 +41,28 @@ export default class Login extends Component {
               request_token: data.request_token,
             }),
           }
-        )
-          .then(response => response.json())
-          .then(data => {
-            fetch(
-              `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,
-              {
-                method: 'POST',
-                mode: 'cors',
-                headers: {
-                  'Content-type': 'application/json',
-                },
-                body: JSON.stringify({
-                  request_token: data.request_token,
-                }),
-              }
-            )
-              .then(response => response.json())
-              .then(data => {
-                console.log('session', data);
-              });
-          });
+        );
+      })
+      .then(data => {
+        return fetchApi(
+          `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,
+          {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+              'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+              request_token: data.request_token,
+            }),
+          }
+        );
+      })
+      .then(data => {
+        console.log('session', data);
+      })
+      .catch(error => {
+        console.log('error', error);
       });
   };
 
