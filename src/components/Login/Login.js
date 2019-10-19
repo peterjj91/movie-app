0fbfafc50b66ab2ce94faf095bbde875b5c0e987
Popeskul Pavel <peterjj91@gmail.com>
2019-10-19 13:42:41 +0300

    Try Async Await Chain for Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -25,6 +25,46 @@ export default class Login extends Component {
       });
     };
 
+    // try {
+    //   const data = await fetchApi(
+    //     `${API_URL}/authentication/token/new?api_key=${API_KEY_3}`
+    //   );
+
+    //   const result = await fetchApi(
+    //     `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`,
+    //     {
+    //       method: 'POST',
+    //       mode: 'cors',
+    //       headers: {
+    //         'Content-type': 'application/json',
+    //       },
+    //       body: JSON.stringify({
+    //         username: 'peter_movie',
+    //         password: '123123',
+    //         request_token: data.request_token,
+    //       }),
+    //     }
+    //   );
+
+    //   const { session_id } = await fetchApi(
+    //     `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,
+    //     {
+    //       method: 'POST',
+    //       mode: 'cors',
+    //       headers: {
+    //         'Content-type': 'application/json',
+    //       },
+    //       body: JSON.stringify({
+    //         request_token: result.request_token,
+    //       }),
+    //     }
+    //   );
+
+    //   console.log('session_id', session_id);
+    // } catch (error) {
+    //   console.log('error', error);
+    // }
+
     fetchApi(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
       .then(data => {
         return fetchApi(
