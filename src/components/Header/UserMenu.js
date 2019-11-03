244aa695385236fe257699fbb0c9310c612c631a
Popeskul Pavel <peterjj91@gmail.com>
2019-11-03 18:46:50 +0200

    LogOut implementation

--- src/components/Header/UserMenu.js
+++ src/components/Header/UserMenu.js
@@ -1,23 +1,68 @@
 import React from 'react';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
 import PropTypes from 'prop-types';
 import AppContextHOC from './../HOC/AppContextHOC';
+import { fetchApi, API_URL, API_KEY_3 } from './../../api/api';
 
 class UserMenu extends React.Component {
+  state = {
+    dropdownOpen: false,
+  };
+
   static propTypes = {
     user: PropTypes.object,
+    session_id: PropTypes.string,
+    onLogOut: PropTypes.func,
+  };
+
+  toggleDropdown = () => {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen,
+    }));
+  };
+
+  handleLogOut = () => {
+    fetchApi(`${API_URL}/authentication/session?api_key=${API_KEY_3}`, {
+      method: 'DELETE',
+      mode: 'cors',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        session_id: this.props.session_id,
+      }),
+    }).then(() => {
+      this.props.onLogOut();
+    });
   };
 
   render() {
     const { user } = this.props;
     return (
-      <div>
-        <img
-          width="40"
-          className="rounded-circle"
-          src={`https://secure.gravatar.com/avatar/${user.avatar.gravatar.hash}.jpg?s=64"`}
-          alt=""
-        />
-      </div>
+      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+        <DropdownToggle
+          tag="div"
+          onClick={this.toggleDropdown}
+          data-toggle="dropdown"
+          aria-expanded={this.state.dropdownOpen}
+        >
+          <img
+            width="40"
+            className="rounded-circle"
+            src={`https://secure.gravatar.com/avatar/${user.avatar.gravatar.hash}.jpg?s=64"`}
+            alt=""
+            onClick={this.toggleDropdown}
+          />
+        </DropdownToggle>
+        <DropdownMenu right>
+          <DropdownItem onClick={this.handleLogOut}>Выйти</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
     );
   }
 }
