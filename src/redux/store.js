2cd1ce448c98e1614ff1153418c9f1ab24002e78
Pavel Popeskul <peterjj91@gmail.com>
2020-01-10 23:59:35 +0200

    Redux: Middleware. Redux-devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,24 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+// const logget = function(store) {
+//   return function(next) {
+//     return function(action) {
+//       return next(action)
+//     }
+//   }
+// }
+const logger = store => next => action => {
+  console.log('logger state', store.getState());
+  console.log('logger', action.type, action.payload);
+  return next(action);
+};
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+  // other store enhancers if any
+);
 
 export default store;
